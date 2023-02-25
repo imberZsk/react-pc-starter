717eb4abfde268ed0be57d377d6e4fbfdc0a7a35
imber <a1157911285@163.com>
2023-02-25 22:54:13 +0800

    feat: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { visualizer } from 'rollup-plugin-visualizer'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  css: {
+    // 开css sourcemap方便找css
+    devSourcemap: true
+  },
+  plugins: [
+    react(),
+    visualizer({
+      open: false // 打包完成后自动打开浏览器，显示产物体积报告
+    })
+  ],
+  server: {
+    // 自动打开浏览器
+    open: true
+  },
+  resolve: {
+    // 配置别名
+    alias: { '@': path.resolve(__dirname, './src') }
+  },
+  //打包路径变为相对路径,用liveserver打开,便于本地测试打包后的文件
+  base: './'
 })
